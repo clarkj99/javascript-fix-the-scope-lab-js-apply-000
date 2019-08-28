2941749e4a70349b4d02a5c32825de2d26789f21
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-08-28 20:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var animal = 'dog'
+var animal = 'dog';
+var otherAnimal = 'cat';
 
 function myAnimal() {
   return animal;
@@ -8,14 +9,13 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return otherAnimal;
 }
 
 function add2(n) {
-  return n + two
-
   // Feel free to move things around!
-  const two = 2
+  const two = 2;
+  return n + two;
 }
 
 var funkyFunction = function() {
